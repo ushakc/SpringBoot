1627ad279d863ae15d42a216a470e55c403171cc
ushakc <34007594+ushakc@users.noreply.github.com>
2017-12-26 22:56:23 -0600

    updated

--- src/main/java/com/student/SpringBoot/Application.java
+++ src/main/java/com/student/SpringBoot/Application.java
@@ -2,6 +2,9 @@ package com.student.SpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.HashMap;
 
@@ -11,14 +14,14 @@ public class Application {
 	public static HashMap<Long,Student> hmStudent;
 
 	public static void main(String[] args) {
-		hmStudent=new HashMap<Long,Student>();
+		hmStudent = new HashMap<Long, Student>();
 
-		Student one=new Student("John","math");
-		hmStudent.put(new Long(one.getId()),one);
+		Student one = new Student("John", "math");
+		hmStudent.put(new Long(one.getId()), one);
 
 		SpringApplication.run(Application.class, args);
 
-		Student two=new Student("Jane","history");
-		hmStudent.put(new Long(two.getId()),two);
+		Student two = new Student("Jane", "history");
+		hmStudent.put(new Long(two.getId()), two);
+		}
 	}
-}
--- src/main/java/com/student/SpringBoot/StudentService.java
+++ src/main/java/com/student/SpringBoot/StudentService.java
@@ -1,30 +1,56 @@
 package com.student.SpringBoot;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
- import java.util.HashMap;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping(value="/rest/student")
-class StudentService{
+class StudentService {
 
-    @RequestMapping(value="/",method = RequestMethod.GET)
-    public HashMap<Long,Student> getAllStudents(){
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public HashMap<Long, Student> getAllStudents() {
         return Application.hmStudent;
     }
 
-    @RequestMapping(value="/add",method = RequestMethod.POST)
-    public Student addStudent(@RequestParam(value="name") String name
-            ,@RequestParam(value="subject",defaultValue = "unknown") String subject){
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public Student addStudent(@RequestParam(value = "name") String name
+            , @RequestParam(value = "subject", defaultValue = "unknown") String subject) {
 
-        Student student=new Student(name,subject);
-        Application.hmStudent.put(new Long(student.getId()),student);
+        Student student = new Student(name, subject);
+        Application.hmStudent.put(new Long(student.getId()), student);
         return student;
 
     }
 
+    @RequestMapping(value="/update", method = RequestMethod.PUT)
+    public Student updateStudent(@RequestBody Student student) throws Exception {
+
+        if(Application.hmStudent.containsKey(new Long(student.getId()))){
+            Application.hmStudent.put(new Long(student.getId()),student);
+        }else{
+            throw new Exception("Student "+student.getId()+" does not exists");
+        }
+
+        return student;
+    }
+    @RequestMapping(value="/delete/{id}",method = RequestMethod.DELETE)
+    public Student deleteStudent(@PathVariable long id) throws Exception {
+
+        Student student;
+
+        if(Application.hmStudent.containsKey(new Long(id))){
+            student=Application.hmStudent.get(new Long(id));
+            Application.hmStudent.remove(new Long(id));
+        }else{
+            throw new Exception("Student "+id+" does not exists");
+        }
+        return student;
+    }
+
+    @RequestMapping(value="/{id}",method = RequestMethod.GET)
+    public Student getStudent(@PathVariable long id){
+        return Application.hmStudent.get(new Long(id));
+    }
 
 }
